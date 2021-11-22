e67796da380b3ad54e12676157aff26ce6002429
TSOYE Jean Robert <tsoyejr@gmail.com>
2021-11-22 09:44:44 +0100

    Ajout config circleCI

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -1,28 +1,63 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { CurrentWeatherComponent } from './current-weather.component';
-import { WeatherServiceFake } from '../weather/weather.service.fake';
+import { fakeWeather, WeatherServiceFake } from '../weather/weather.service.fake';
 import { WeatherService } from '../weather/weather.service';
+import { injectSpy } from 'angular-unit-test-helper';
+import { of } from 'rxjs/internal/observable/of';
 
 describe('CurrentWeatherComponent', () => {
   let component: CurrentWeatherComponent;
   let fixture: ComponentFixture<CurrentWeatherComponent>;
+  let weatherServiceMock: jasmine.SpyObj<WeatherService>;
 
   beforeEach(async () => {
+    const weatherServiceSpy = jasmine.createSpyObj('WeatherService', ['getCurrentWeather']);
     await TestBed.configureTestingModule({
       declarations: [ CurrentWeatherComponent ],
-      providers: [ { provide: WeatherService, useClass: WeatherServiceFake } ],
+      providers: [ { provide: WeatherService, useValue: weatherServiceSpy } ],
     })
     .compileComponents();
+    weatherServiceMock = injectSpy(WeatherService);
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CurrentWeatherComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of());
+    // Act
+    fixture.detectChanges(); // triggers ngOnInit
+    // Assert
     expect(component).toBeTruthy();
+  });
+
+  it('should get currentWeather from weatherService', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of());
+    // Act
+    fixture.detectChanges(); // triggers ngOnInit
+    // Assert
+    expect(weatherServiceMock.getCurrentWeather)
+      .toHaveBeenCalledTimes(1);
+  });
+
+  it('should eagerly load currentWeather in Yaoundé from weatherService', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of(fakeWeather));
+    // Act
+    fixture.detectChanges(); // triggers ngOnInit
+    // Assert
+    expect(component.current).toBeDefined();
+    expect(component.current.city).toEqual('Yaoundé');
+    expect(component.current.temperature).toEqual(280.32);
+    // Assert on DOM
+    const debugEl = fixture.debugElement;
+    const titleEl: HTMLElement = debugEl.query(By.css('span')).nativeElement;
+    expect(titleEl.textContent).toContain('Yaoundé');
   });
 });
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  appId: '85118692000cfec3cb919c6b5444e923',
+  baseUrl: 'https://'
 };
